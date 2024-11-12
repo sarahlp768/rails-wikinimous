f6efefc78dde70e064220b7001cec65cc7a7d995
Sarah Lepilleur <sarah.lepilleur@sciencespo.fr>
2024-11-12 15:44:48 +0100

    nov 12 exercise 3 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Company.name,
+    content: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+  )
+  article.save!
+end
+puts 'Finished!'
